5ffac66f8e7278be1397618721fd83d5aad47888
Arindam Nandi <arinandi2001@gmail.com>
2023-08-06 12:27:11 +0530

    Almost There..

--- src/App.js
+++ src/App.js
@@ -6,6 +6,12 @@ import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import Alert from './components/Alert';
 
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+
 function App() {
   const [mode, setMode] = useState('light');  // whether dark mode is enabled or not.
   const [alert, setAlert] = useState(null);
@@ -25,11 +31,13 @@ function App() {
       setMode('dark');
       document.body.style.backgroundColor = '#042c6b';
       showAlert("Dark Mode has been Activated.", "success");
+      document.title = "TextUtils - Dark Mode"
     }
     else {
       setMode('light');
       document.body.style.backgroundColor = 'white';
       showAlert("Light Mode has been Activated.", "primary");
+      document.title = "TextUtils - Light Mode"
     }
 
   }
@@ -37,12 +45,22 @@ function App() {
   return (
     <>
       {/* <Navbar/> */}
-      <Navbar title="TextUtils" aboutText="About TextUtils" mode={mode} toggleMode={toggleMode} />
-      <Alert alert={alert} />
-      <div className="container my-3">
-        <TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode} />
-        {/* <About /> */}
-      </div>
+      <Router>
+        <Navbar title="TextUtils" aboutText="About TextUtils" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Routes>
+            {/* component 1 - /users
+              component 2 - /users/home
+              React do partial matching so if We don't use "exact path" instead of going to /users/home it'll go to users
+           */}
+            <Route exact path='/' element={<TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode} />} />
+            <Route exact path='/about' element={<About />} />
+          </Routes>
+
+
+        </div>
+      </Router>
 
     </>
   );
